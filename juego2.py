172000304a0ecb40424be6f072febc7e72412dbb
andmansim <91722324+andmansim@users.noreply.github.com>
2021-11-09 21:18:54 +0100

    commit 2

--- juego2.py
+++ juego2.py
@@ -1,5 +1,11 @@
-nivel_sencillo = 0
-nivel_medio = 1
-nivel_avanzado = 2
-nivel_experto = 3
+def game_level():
+    nivel_sencillo = 0
+    nivel_medio = 1
+    nivel_avanzado = 2
+    nivel_experto = 3
+    levels = ["Game levels:" + "nivel_sencillo: " + str(nivel_sencillo), "nivel_medio: " + str(nivel_medio), "nivel_avanzado: " + str(nivel_avanzado), "nivel_experto: " + str(nivel_experto)]
+    print(levels)
+    user = int(input("Choose one level for the game: "))
+    return user
+
 
