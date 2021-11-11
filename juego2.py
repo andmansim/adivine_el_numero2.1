fc15735ae30613399ee28374e3932247e2d280bd
andmansim <91722324+andmansim@users.noreply.github.com>
2021-11-11 22:16:41 +0100

    commit

--- juego2.py
+++ juego2.py
@@ -1,3 +1,13 @@
+
+i = 1
+j = 2
+result = []
+for n in range(i):
+    a = [0]*j
+    result.append(a)
+  
+result2=list()
+
 def game_level():
     nivel_sencillo = 0
     nivel_medio = 1
@@ -27,6 +37,31 @@ def pedir_numero(value_min, value_max, attempts):
         valor_usuario = int(input())
     if valor_usuario == number:
         print("¡Congratulations!, you have completed the level")
+        print(("Put your name: "))
+        name = str(input())
+        #b = [name] * [0]
+        #result.append(b)
+       
+        index=-1 
+        for m in range (len(result)):
+            if result[m][0]== name:
+                index=m
+                        
+        print(index)
+        if index == -1:
+            a = [0]*2
+            
+            result.append(a)
+            
+            element=len(result)
+            result[element-1][0]=name
+            result[element-1][1]=1
+            
+        else:
+            result[index][0]=name
+            result[index][1]=result[index][1]+1
+              
+    
     if rest_attempts == 0 and valor_usuario != number:
         print("Game over")
 
@@ -34,7 +69,7 @@ def jugar():
     user_choice = game_level()
     continue_playing = True 
     while continue_playing == True:
-        print(continue_playing)
+        
         if user_choice == 0:
             pedir_numero(0,100,5)
         elif user_choice == 1:
@@ -49,8 +84,8 @@ def jugar():
         if new_intent != str('Yes'):
             continue_playing = False
                 
-
 jugar()
+print(result)
                 
     
 
